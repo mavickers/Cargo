f3844c0e7c9634e6ea4451f52fa26e2db5d3d2d3
Michael A. Vickers <mavickers@lightpathllc.com>
2021-10-04 08:41:56 -0400

    Rename _package -> _contents.

--- Cargo/Package.cs
+++ Cargo/Package.cs
@@ -21,10 +21,10 @@ namespace LightPath.Cargo
         private Exception _exception { get; }
         private Guid _executionId { get; }
         private readonly ILogger<T> _logger;
-        private readonly T _package;
+        private readonly T _contents;
 
         public Exception AbortedWith => _abortedWith;
-        public T Contents => _package;
+        public T Contents => _contents;
         public Exception Exception => _exception;
         internal Guid ExecutionId => _executionId;
         public bool IsAborted => _abort;
@@ -39,10 +39,10 @@ namespace LightPath.Cargo
             if (parameters.Count(p => p?.GetType() == typeof(T)) != 1) throw new ArgumentException($"Package parameters must contain a single instance of {typeof(T).FullName}");
 
             _abort = false;
+            _contents = (T)parameters.First(p => p.GetType() == typeof(T));
             _exception = null;
             _executionId = Guid.NewGuid();
             _logger = (ILogger<T>) parameters.FirstOrDefault(p => p is ILogger<T>) ?? new Logger<T>(new NullLoggerFactory());
-            _package = (T)parameters.First(p => p.GetType() == typeof(T));
 
             Results = new List<Station.Result<T>>();
         }
